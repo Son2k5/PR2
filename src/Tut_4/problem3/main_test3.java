83fa1a3734661acb7e417ef2790b49ce846bbb75
Son2k5 <sonct2k3@gmail.com>
2025-02-24 10:52:54 +0700

    upload tutorial 4

---
+++ src/Tut_4/problem3/main_test3.java
@@ -0,0 +1,21 @@
+package Tut_4.problem3;
+
+import java.awt.*;
+
+public class main_test3 {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(1.2f, 3.4f);
+        System.out.println(r1);
+        Rectangle r2 = new Rectangle();
+        System.out.println(r2);
+
+        r1.setHeight(5.6f);
+        r1.setWidth(7.8f);
+        System.out.println(r1);
+        System.out.printf("Length is: %.2f%n", r1.getHeight());
+        System.out.printf("Width is: %.2f%n", r1.getWidth());
+
+        System.out.printf("Area is: %.2f%n", r1.getArea());
+        System.out.printf("Perimeter is: %.2f%n", r1.getPerimeter());
+    }
+}
